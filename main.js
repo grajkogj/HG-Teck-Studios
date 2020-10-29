9e163c093298c78ad39cad059147e9dea20621c4
grajkogj <67935829+grajkogj@users.noreply.github.com>
2020-10-29 11:10:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-document.getElementById("header-home-button").addEventListener('click', function() {window.location.href = "home.html"})
-document.getElementById("header-services-button").addEventListener('click', function() {window.location.href = "services.html"})
+document.getElementById("header-home-button").addEventListener('click', function() {window.location.href = "home.html"});
+document.getElementById("header-services-button").addEventListener('click', function() {window.location.href = "services.html"});
+document.getElementById("our-services-link").addEventListener('click', function() {window.location.href = "services.html"});
